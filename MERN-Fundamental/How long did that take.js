01b4795f0f96a41f15953185f19a44e44c3a7bdc
mutaz jaber <mutaz-mhj@hotmail.com>
2024-02-05 00:47:35 +0200

    more optional

--- MERN-Fundamental/How long did that take.js
+++ MERN-Fundamental/How long did that take.js
@@ -33,6 +33,40 @@
 // console.log(`This took ${performance.now() - start} milliseconds to run`);
 
 
-const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
-const reversed1 = story.split("").reverse().join("");
-console.log(reversed1);
+
+
+const { performance } = require('perf_hooks');
+
+// recursive 
+function rFib( n ) {
+    if ( n < 2 ) {
+        return n;
+    }
+    return rFib( n - 1 ) + rFib( n - 2 );
+}
+
+// iterative 
+function iFib( n ) {
+    const vals = [ 0, 1 ];
+    while (vals.length - 1 < n) {
+        let len = vals.length;
+        vals.push( vals[len - 1] + vals[len - 2] );
+    }
+    return vals[n];
+}
+
+//  recursive O
+const startRecursive = performance.now();
+rFib(20);
+const endRecursive = performance.now();
+console.log(`recursive : ${endRecursive - startRecursive} milliseconds`);
+
+// Measure the time taken by the iterative Fibonacci function
+const startIterative = performance.now();
+iFib(20);
+const endIterative = performance.now();
+console.log(`iterative: ${endIterative - startIterative} milliseconds`);
+
+// const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
+// const reversed1 = story.split("").reverse().join("");
+// console.log(reversed1);
